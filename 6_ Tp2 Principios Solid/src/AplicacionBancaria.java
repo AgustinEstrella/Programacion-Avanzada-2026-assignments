79ebc64c1c8c85311c2f2bc67c8deb5ecae44152
Agustin Estrella notebook <agustin.j.estrella@gmail.com>
2026-05-21 15:55:59 -0300

    Cambie un poco la redaccion de los comentarios

--- 6_ Tp2 Principios Solid/src/AplicacionBancaria.java
+++ 6_ Tp2 Principios Solid/src/AplicacionBancaria.java
@@ -1,12 +1,10 @@
-import java.util.spi.CalendarDataProvider;
-
 public class AplicacionBancaria {
     public static void main(String[] args){
 
-        //Expandimos CuentaBancaria ya que corrompia el principio de abierto/cerrado
-        //abrimos la funcion retirar para que se pueda ampliar pero no modificar,
+        //Expandimos CuentaBancaria ya que corrompía el principio de abierto/cerrado
+        //abrimos la función retirar para que se pueda ampliar, pero no modificar,
         //dividiendo la clase en CuentaCorriente y CajaAhorro, teniendo cada uno su manera de retirar
-        //la clase CuentaBancaria no se cambia, sino que se puede cambiar la logica de cada tipo de cuenta
+        //la clase CuentaBancaria no se cambia, sino que se puede cambiar la lógica de cada tipo de cuenta
         CuentaBancaria cuenta1 = new CuentaCorriente("Pepe", 20000, 1, 10000);
         cuenta1.depositar(500);
         cuenta1.retirar(200);
@@ -15,19 +13,16 @@ public class AplicacionBancaria {
         cuenta2.depositar(500);
         cuenta2.retirar(200);
 
-        //Creamos la clase ImpresoraCuenta porque rompia el principio de
+        //Creamos la clase ImpresoraCuenta e InboxMail porque se rompía el principio de
         //responsabilidad unica, a la cuenta bancaria no se le debe
-        //atribuir el funcionamiento de un sistema de impresion
+        //atribuir el funcionamiento de un sistema de impresion ni de envío de e-mails
         ImpresoraCuenta impresora = new ImpresoraCuenta();
         impresora.mostrarDatosCuenta(cuenta1);
         impresora.mostrarDatosCuenta(cuenta2);
 
-        //Creamos la clase InboxMail porque rompia el principio de
-        //responsabilidad unica, a la cuenta bancaria no se le debe
-        //atribuir el funcionamiento de un sistema de e-mails
         InboxMail inbox = new InboxMail();
-        inbox.enviarNotificacionPorMail(cuenta1, "Notificacion enviada!");
-        inbox.enviarNotificacionPorMail(cuenta2, "Notificacion enviada!");
+        inbox.enviarNotificacionPorMail(cuenta1, "Notificación enviada!");
+        inbox.enviarNotificacionPorMail(cuenta2, "Notificación enviada!");
 
     }
 }
